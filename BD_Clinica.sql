d0e77700e4f768aedfc12114d159668025c44bb9
g0dspee <134010619+g0dspee@users.noreply.github.com>
2023-11-21 23:59:21 -0300

    Add files via upload

--- BD_Clinica.sql
+++ BD_Clinica.sql
@@ -23,17 +23,18 @@ CREATE TABLE `paciente` (
   `apellido` varchar(25) NOT NULL,
   `sexo` char(1) NOT NULL,
   `fecha_nacimiento` date NOT NULL,
-  `num_telefono` int NOT NULL,
   `prevision_salud` varchar(20) NOT NULL,
   `grupo_sanguineo` varchar(5) NOT NULL,
   `paciente_activo` tinyint(1) NOT NULL,
   `unidad_medica` varchar(20) NOT NULL,
+  `defuncion` tinyint(1) NOT NULL,
+  `evolucion` varchar(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT ' ',
   KEY `rut_paciente` (`rut_paciente`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 /*Data for the table `paciente` */
 
-insert into `paciente` (`rut_paciente`,`nombre`,`apellido`,`sexo`,`fecha_nacimiento`,`num_telefono`,`prevision_salud`,`grupo_sanguineo`,`paciente_activo`,`unidad_medica`) values ('201234567','Mauricio','Ross','M','1991-05-11',912345678,'Isapre Colmena','A+',1,'Hospitalizado');
-insert into `paciente` (`rut_paciente`,`nombre`,`apellido`,`sexo`,`fecha_nacimiento`,`num_telefono`,`prevision_salud`,`grupo_sanguineo`,`paciente_activo`,`unidad_medica`) values ('202345678','Pol','Miranda','M','1999-08-04',923456789,'Fonasa','O+',1,'Transitorio');
-insert into `paciente` (`rut_paciente`,`nombre`,`apellido`,`sexo`,`fecha_nacimiento`,`num_telefono`,`prevision_salud`,`grupo_sanguineo`,`paciente_activo`,`unidad_medica`) values ('112223334','Millaray','Gonzalez','F','2003-02-01',912129898,'Isapre Cruz Blanca','AB-',1,'Urgencia');
-insert into `paciente` (`rut_paciente`,`nombre`,`apellido`,`sexo`,`fecha_nacimiento`,`num_telefono`,`prevision_salud`,`grupo_sanguineo`,`paciente_activo`,`unidad_medica`) values ('8234123K','Ricardo','Stallman','M','1953-03-16',972746892,'Fonasa','B-',0,'Hospitalizado');
+insert into `paciente` (`rut_paciente`,`nombre`,`apellido`,`sexo`,`fecha_nacimiento`,`prevision_salud`,`grupo_sanguineo`,`paciente_activo`,`unidad_medica`,`defuncion`,`evolucion`) values ('201234567','Mauricio','Ross','M','1991-11-05','Isapre Colmena','A+',1,'Hospitalizado',0,'');
+insert into `paciente` (`rut_paciente`,`nombre`,`apellido`,`sexo`,`fecha_nacimiento`,`prevision_salud`,`grupo_sanguineo`,`paciente_activo`,`unidad_medica`,`defuncion`,`evolucion`) values ('202345678','Pol','Miranda','M','1999-08-04','Fonasa','O+',1,'Transitorio',0,'');
+insert into `paciente` (`rut_paciente`,`nombre`,`apellido`,`sexo`,`fecha_nacimiento`,`prevision_salud`,`grupo_sanguineo`,`paciente_activo`,`unidad_medica`,`defuncion`,`evolucion`) values ('112223334','Millaray','Gonzalez','F','2003-01-02','Isapre Cruz Blanca','AB-',1,'Urgencia',0,'');
+insert into `paciente` (`rut_paciente`,`nombre`,`apellido`,`sexo`,`fecha_nacimiento`,`prevision_salud`,`grupo_sanguineo`,`paciente_activo`,`unidad_medica`,`defuncion`,`evolucion`) values ('8234123K','Ricardo','Stallman','M','1953-03-16','Fonasa','B-',0,'Hospitalizado',0,'');
